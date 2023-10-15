d3a168d8818a37d9119d042f037d3a3177f39673
Lukas Nielsen <moin@lukasnielsen.de>
2023-10-15 12:46:59 +0200

    remove webapi

--- webapi/const.go
+++
@@ -1,8 +0,0 @@
-package webapi
-
-const (
-	CHAYNS_paymentSites              = "https://webapi.tobit.com/AccountService/v1.0/Chayns/PaymentSites"
-	OPM_charge_leftAmount            = "https://webapi.tobit.com/PaymentProcessors/OPM/Charge/LeftAmount"
-	CHAYNS_user                      = "https://webapi.tobit.com/AccountService/v1.0/Chayns/User"
-	OPM_personData_publicInformation = "https://webapi.tobit.com/AccountService/v1.0/OPM/PersonData/PublicInformation?perssonId=@@chaynsId@@"
-)
